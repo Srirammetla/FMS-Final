12b7d89f1f856bcae3d2642c052197e834f5b47c
Metla Sriram <metla.sriram@lumen.com>
2024-01-12 22:33:49 +0530

    Template Project

---
+++ src/main/java/com/sriram/fmsfinal/FmsFinalApplication.java
@@ -0,0 +1,13 @@
+package com.sriram.fmsfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsFinalApplication.class, args);
+	}
+
+}
